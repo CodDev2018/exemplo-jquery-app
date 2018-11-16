3fa548c81cc29a1427632169ac2b90281d807f34
Codinome Developer <codinomedeveloper@gmail.com>
2018-11-16 09:53:40 -0300

    utlizando jquery na page

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,4 +1,13 @@
-
-var navegarPara = function(div, page) {
+var navegarPara = function (div, page) {
     $(div).load(page);
 }
+
+var exibeMensagem = function (div, mensagem) {
+    $(div).html(mensagem);
+}
+
+$(function () {
+    $('#bnt_msg').click(function () {
+        alert('Minha mensagem!')
+    })
+})
